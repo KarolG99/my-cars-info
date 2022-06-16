58776451412c0719becf002f59afda299d148c27
KarolG99 <karolg@MacBook-Pro-Karol.local>
2022-06-16 15:34:35 +0200

    added navigation

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem 1.5rem;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { StyledMain, Wrapper } from "./App.styles";
+import { GlobalStyle } from "./assets/styles/GlobalStyles";
+import AddCar from "./components/AddCar/AddCar";
+import AllCars from "./components/AllCars/AllCars";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <StyledMain>
+        <Routes>
+          <Route path="/" element={<AllCars />} />
+          <Route path="/add-car" element={<AddCar />} />
+        </Routes>
+      </StyledMain>
+
+      <Navigation />
+    </Wrapper>
   );
 }
 
