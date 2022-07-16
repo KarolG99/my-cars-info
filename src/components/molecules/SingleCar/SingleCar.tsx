7bc91a7e86064e77b941c2cbd1c5b7e59e7cb07e
KarolG99 <karolus2212@gmail.com>
2022-07-16 10:13:57 +0200

    contribution test 2

--- src/components/molecules/SingleCar/SingleCar.tsx
+++ src/components/molecules/SingleCar/SingleCar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import { CarsProps } from "../../../types";
 import { StyledButton } from "../../atoms/StyledButton";
 import { ButtonsWrapper } from "../../organisms/MainPage/MainPage.styles";
